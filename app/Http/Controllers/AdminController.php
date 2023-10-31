a907328fc24bb438e8fd63eb48776a4fa9054478
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2023-11-01 01:57:57 +0700

    usercontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
@@ -11,16 +13,14 @@ class AdminController extends Controller
     }
 
     public function user() {
-        $users = Users::all();
-        $kotas = Kota::all();
-        $roles = Role::all();
+        $users = User::all();
         return view('users.index',['users' => $users], compact('kotas','roles'));
     }
     
     public function create(Request $request){
  
         //insert ke table user
-        Users::create([
+        User::create([
 
             'id_kota' => request('id_kota'),
             'id_role' => request('id_role'),
@@ -35,30 +35,30 @@ class AdminController extends Controller
       return redirect ('/users')->with('sukses','Data Berhasil Diinput');
     }
     public function edit ($id_user){
-        $users = \App\Models\Users::find($id_user);
+        $users = \App\Models\User::find($id_user);
         return view('users/edit',['users' => $users]);
     }
   
     public function update (Request $request,$id_user){
-        $user = \App\Models\Users::find($id_user);
+        $user = \App\Models\User::find($id_user);
         $user->update($request->all());
         return redirect('/users')->with('sukses','Data Berhasil diupdate');
     }
   
     public function delete ($id_user){
-        $user = \App\Models\Users::find($id_user);
+        $user = \App\Models\User::find($id_user);
         $user->delete($user);
         return redirect('/users')->with('sukses','Data Berhasil dihapus');
     }
   
     public function trash(){
-      $user = Users::onlyTrashed()->get();
+      $user = User::onlyTrashed()->get();
       return view('users.trash',['user' => $user]);
   }
   
   public function restore($id_user = null){
       if($id_user != null){
-          $user = Users::onlyTrashed()
+          $user = User::onlyTrashed()
           ->where('id_user', $id_user)
           ->restore();
       }
@@ -67,7 +67,7 @@ class AdminController extends Controller
   
   public function forceDelete($id_user = null){
       if($id_user != null){
-          $user = Users::onlyTrashed()
+          $user = User::onlyTrashed()
           ->where('id_user', $id_user)
           ->forceDelete();
       }
